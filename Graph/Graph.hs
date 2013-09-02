b566f7097205e260968f2bc79d0ab20172b09c9f
Taylor E Berger <taylor@taylor-workstation.(none)>
2013-09-02 14:44:25 -0600

    Moved undirected graph to it's own file

--- Graph/Graph.hs
+++ Graph/Graph.hs
@@ -1,31 +1,40 @@
-{-# LANGUAGE GADTs, MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
-module Network(Edge, 
-               to, 
-               from, 
-               val, 
-               ) where
+{-# LANGUAGE GADTs, FunctionalDependencies, FlexibleInstances #-}
+module Graph(Edge, 
+             makeEdge,
+             to, 
+             from, 
+             val,
+             Graph,
+             addVertex,
+             addVertices,
+             removeVertex,
+             removeVertices,
+             addEdge,
+             addEdges,  
+             removeEdge,
+             removeEdges,
+             getVertices,
+             getEdges,
+             outEdges,
+             inEdges,        
+            module Data.List,
+            ) where
 
 import Data.List
-import Data.Maybe
 import qualified Data.Set as Set
 
-data Vertex a = V {v :: a,
-                   iden :: Int
-                  } deriving Show
-                
-instance Eq (Vertex a) where 
-  a == b = (iden a) == (iden b)
-  
-instance Ord (Vertex a) where
-  a < b = (iden a) < (iden b)
 
+-- Edge data constructor and instance delcarations
 data Edge a b = (Eq b) => Edge {to   :: a, 
                                 from :: a,
                                 val  :: b
                                }
-
+                
+makeEdge :: (Eq a, Eq b) => a -> a -> b -> Edge a b
+makeEdge t f v = Edge t f v
+  
 instance (Eq b, Eq a) => Eq (Edge a b) where                
-  (==) a b = (to a) == (to b) && (from a) == (from b)
+  (==) a b = (to a) == (to b) && (from a) == (from b) && (val a) == (val b)
   
 instance (Ord v, Eq e) => (Ord (Edge v e)) where
   e1 < e2 = (to e1) < (to e2)
@@ -34,24 +43,24 @@ instance (Ord v, Eq e) => (Ord (Edge v e)) where
   e1 <= e2 = (to e1) <= (to e2)
   
 instance (Show v, Show e) => Show (Edge v e) where
-  show e = "//Edge to: " ++ (show $ to e) ++ ", Edge from: " ++ (show $ from e) ++ ", val: " ++ (show $ val e) ++ "//"
+  show e = " [" ++ (show $ val e) ++ "]={" ++ (show $ to e) ++ "}->{" ++ (show $ from e) ++ "}"
 
+
+-- Graph class starts here
 class Graph g v e | g -> v e where
-  addVertex      :: g ->  v -> g
-  addVertices    :: g -> [v] -> g
-  removeVertex   :: g ->  v  -> g
-  removeVertices :: g -> [v] -> g
-  addEdge        :: g ->  Edge v e  -> g
-  addEdges       :: g -> [Edge v e] -> g
-  removeEdge     :: g ->  Edge v e  -> g
-  removeEdges    :: g -> [Edge v e] -> g
-  getVertices    :: g -> Set.Set v
-  getEdges       :: g -> Set.Set (Edge v e)
-  outEdges       :: g -> v -> Set.Set (Edge v e)
-  inEdges        :: g -> v -> Set.Set (Edge v e)
-  
+  addVertex, removeVertex     :: g ->  v -> g
+  addVertices, removeVertices :: g -> [v] -> g
+  addEdge, removeEdge         :: g ->  Edge v e  -> g
+  addEdges, removeEdges       :: g -> [Edge v e] -> g
+  getVertices                 :: g -> Set.Set v
+  getEdges                    :: g -> Set.Set (Edge v e)
+  inEdges, outEdges           :: g -> v -> Set.Set (Edge v e)
 
-data UndirectedGraph v e = UG {verts :: Set.Set v,
+
+toGML :: (Graph g v e) => g ->  String
+toGML = undefined
+
+{-data UndirectedGraph v e = UG {verts :: Set.Set v,
                                edges :: Set.Set (Edge v e)
                               }
                            
@@ -72,3 +81,5 @@ instance (Ord e, Ord v) => Graph (UndirectedGraph v e) v e where
   removeEdges = foldl' removeEdge
   getEdges = edges
   getVertices = verts
+  inEdges g v = Set.filter ((== v) . to) (edges g)
+  outEdges g v = Set.filter ((== v) . from) (edges g)-}
---
+++ Graph/UndirectedGraph.hs
@@ -0,0 +1,39 @@
+{-# Language FlexibleInstances, FunctionalDependencies #-}
+
+module UndirectedGraph(empty,
+                       forest,
+                       UndirectedGraph,
+                       ) where 
+
+import qualified Data.Set as Set
+import Graph
+
+data UndirectedGraph v e = UG {verts :: Set.Set v,
+                               edges :: Set.Set (Edge v e)
+                              }
+                           
+empty :: (Ord v, Ord e) => UndirectedGraph v e                           
+empty = UG Set.empty Set.empty
+
+forest :: (Ord v, Ord e) => [v] -> UndirectedGraph v e
+forest ls = addVertices empty ls
+                           
+instance (Ord e, Ord v) => Graph (UndirectedGraph v e) v e where
+  addVertex g v = UG (Set.insert v vertices) (edges g)
+    where vertices = verts g
+  addVertices = foldl' addVertex
+  removeVertex g v = UG newVerts newEdges
+    where newVerts = Set.delete v (verts g)
+          newEdges = Set.filter (\e -> not $ any (== v) [to e, from e]) (edges g)
+  removeVertices = foldl' removeVertex
+  addEdge g e = UG vertices newEdges
+    where vertices = Set.insert (from e) $ Set.insert (to e) (verts g)
+          newEdges = Set.insert reverseEdge $ Set.insert e (edges g)
+          reverseEdge = makeEdge (from e) (to e) (val e)
+  addEdges = foldl' addEdge
+  removeEdge g e = UG (verts g) (Set.delete e (edges g))
+  removeEdges = foldl' removeEdge
+  getEdges = edges
+  getVertices = verts
+  inEdges g v = Set.filter ((== v) . to) (edges g)
+  outEdges g v = Set.filter ((== v) . from) (edges g)
