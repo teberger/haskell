2748e450a8d8214d23f0be27d0c1303f04d4727e
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-04 20:28:35 -0700

    /home/tberger/haskell/bayes_classifier/bayesClassifier.hs autocommit from emacs

--- bayes_classifier/bayesClassifier.hs
+++ bayes_classifier/bayesClassifier.hs
@@ -15,13 +15,13 @@ main = do
   vocab:dataLoc:_ <- getArgs
   vocabulary <- liftM (`zip` [1..]) . fmap lines $ hGetContents =<< openFile vocab ReadMode
   
-  train_data  <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.data" ) ReadMode
-  train_label <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.label") ReadMode  
-  train_map   <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.map"  ) ReadMode
+  train_data_lines  <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.data" ) ReadMode
+  train_label_lines <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.label") ReadMode  
+  train_map_lines   <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.map"  ) ReadMode
   
-  test_data  <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.data" ) ReadMode
-  test_label <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.label") ReadMode  
-  test_map   <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.map"  ) ReadMode
+  test_data_lines  <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.data" ) ReadMode
+  test_label_lines <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.label") ReadMode  
+  test_map_lines   <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.map"  ) ReadMode
 
   
 
