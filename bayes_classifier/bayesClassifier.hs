9ccf3f6502696c24f77f617f41ec1f5947302d70
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-04 20:20:08 -0700

    /home/tberger/haskell/bayes_classifier/bayesClassifier.hs autocommit from emacs

--- bayes_classifier/bayesClassifier.hs
+++ bayes_classifier/bayesClassifier.hs
@@ -2,9 +2,9 @@ module Main where
 
 import System.Environment
 import System.IO
-import Control.Monad(liftM)  
+import Control.Monad (liftM)
 
-type Document = [(DocumentNumber, WordIndex, WordCount)]
+type Document = [(WordIndex, WordCount)]
 type DocumentNumber = Int
 type WordIndex = Int
 type WordCount = Int
@@ -14,8 +14,14 @@ main = do
   vocab:dataLoc:_ <- getArgs
   vocabulary <- liftM (`zip` [1..]) . fmap lines $ hGetContents =<< openFile vocab ReadMode
   
-  train_data <- liftM
+  train_data  <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.data" ) ReadMode
+  train_label <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.label") ReadMode  
+  train_map   <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.map"  ) ReadMode
   
+  test_data  <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.data" ) ReadMode
+  test_label <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.label") ReadMode  
+  test_map   <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.map"  ) ReadMode
+
   
   return ()
 
