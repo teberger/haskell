8e50aefea6e72837e6d7833542f6e2b97ccedee6
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-04 19:45:57 -0700

    /home/tberger/haskell/bayes_classifier/bayesClassifier.hs autocommit from emacs

--- bayes_classifier/bayesClassifier.hs
+++ bayes_classifier/bayesClassifier.hs
@@ -1,10 +1,12 @@
 module Main where
 
 import System.Environment
-
+import System.IO
+  
 main :: IO ()
 main = do
   [vocab:dataLoc:_] <- getArgs
+  vocabulary <- lift (`zip` [1..]) $ hContents =<< openFile vocab
   return ()
 
 
