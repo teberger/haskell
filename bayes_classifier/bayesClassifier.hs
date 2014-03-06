e67deebf7022e47e0fd8677d253485e8810f5650
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-06 11:07:09 -0700

    /home/tberger/haskell/bayes_classifier/bayesClassifier.hs autocommit from emacs

--- bayes_classifier/bayesClassifier.hs
+++ bayes_classifier/bayesClassifier.hs
@@ -15,13 +15,15 @@ main = do
   vocab:dataLoc:_ <- getArgs
   vocabulary <- liftM (`zip` [1..]) . fmap lines $ hGetContents =<< openFile vocab ReadMode
   
-  train_data_lines  <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.data" ) ReadMode
+  train_data_lines  <- liftM groupBy ((==) . (!! 0) $ fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.data" ) ReadMode
   train_label_lines <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.label") ReadMode  
   train_map_lines   <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.map"  ) ReadMode
   
-  test_data_lines  <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.data" ) ReadMode
+  test_data_lines  <- liftM groupBy ((==) . (!! 0) $ fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.data" ) ReadMode
   test_label_lines <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.label") ReadMode  
   test_map_lines   <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.map"  ) ReadMode
+  
+  
 
 --  let train_data_temp = 
 --      train_data = map (groupBy ((==) . fst)) 
