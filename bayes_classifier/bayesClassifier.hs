fb8fe60bc38f6c6c0551cab1dfdada3c829a8d6c
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-06 11:15:15 -0700

    /home/tberger/haskell/bayes_classifier/bayesClassifier.hs autocommit from emacs

--- bayes_classifier/bayesClassifier.hs
+++ bayes_classifier/bayesClassifier.hs
@@ -23,7 +23,8 @@ main = do
   test_label_lines <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.label") ReadMode
   test_map_lines   <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.map"  ) ReadMode
   
-  let train_data_temp = groupBy ((==) . (\x -> [x]) . (!! 0)) train_data_lines
+  let f = ((==) . (!! 0)) :: String -> String -> Bool
+      train_data_temp = groupBy ((==) . (!! 0)) train_data_lines
 
   print $ train_data_temp !! 0
   return ()
