717e6d1bdf02ff33eebfa740a7c64d0863bf69b7
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-06 11:11:07 -0700

    /home/tberger/haskell/bayes_classifier/bayesClassifier.hs autocommit from emacs

--- bayes_classifier/bayesClassifier.hs
+++ bayes_classifier/bayesClassifier.hs
@@ -15,11 +15,11 @@ main = do
   vocab:dataLoc:_ <- getArgs
   vocabulary <- liftM (`zip` [1..]) . fmap lines $ hGetContents =<< openFile vocab ReadMode
   
-  train_data_lines  <- liftM (groupBy ((==) . (\x -> [x]) . (!! 0))) $ fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.data" ) ReadMode
+  train_data_lines  <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.data" ) ReadMode
   train_label_lines <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.label") ReadMode  
   train_map_lines   <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.map"  ) ReadMode
   
-  test_data_lines  <- liftM (groupBy ((==) . (\x -> [x] ). (!! 0))) $ fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.data" ) ReadMode
+  test_data_lines  <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.data" ) ReadMode
   test_label_lines <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.label") ReadMode  
   test_map_lines   <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.map"  ) ReadMode
   
