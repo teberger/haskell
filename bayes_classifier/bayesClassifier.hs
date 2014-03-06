350b87d334ba1e257d317b08aa7d9d7bbeb73ea5
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-06 11:08:26 -0700

    /home/tberger/haskell/bayes_classifier/bayesClassifier.hs autocommit from emacs

--- bayes_classifier/bayesClassifier.hs
+++ bayes_classifier/bayesClassifier.hs
@@ -15,7 +15,7 @@ main = do
   vocab:dataLoc:_ <- getArgs
   vocabulary <- liftM (`zip` [1..]) . fmap lines $ hGetContents =<< openFile vocab ReadMode
   
-  train_data_lines  <- liftM (groupBy ((==) . (!! 0))) $ fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.data" ) ReadMode
+  train_data_lines  <- liftM (groupBy ((==) . [] . (!! 0))) $ fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.data" ) ReadMode
   train_label_lines <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.label") ReadMode  
   train_map_lines   <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "train.map"  ) ReadMode
   
