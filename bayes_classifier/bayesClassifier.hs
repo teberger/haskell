e985b1e387bf20e9d3a8531af1561360bcc334c8
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-08 10:18:14 -0700

    /home/tberger/haskell/bayes_classifier/bayesClassifier.hs autocommit from emacs

--- bayes_classifier/bayesClassifier.hs
+++ bayes_classifier/bayesClassifier.hs
@@ -31,7 +31,7 @@ main = do
       testDocs  = map makeDoc test_data_temp
       testData  = zip test_label_lines testDocs
 
-  print $ length trainDocs
+  print $ length train_data_temp
   return ()
 
 makeDoc :: [String] -> Document
