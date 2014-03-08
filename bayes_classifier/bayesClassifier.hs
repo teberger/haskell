53cdfd03c9b2c909811e84166fb5a08fc2a913ae
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-08 10:58:29 -0700

    /home/tberger/haskell/bayes_classifier/bayesClassifier.hs autocommit from emacs

--- bayes_classifier/bayesClassifier.hs
+++ bayes_classifier/bayesClassifier.hs
@@ -7,12 +7,16 @@ import Data.List
 import Data.Array.Base
 import Data.Array.Unboxed
 
-type Instance = (Label, Document)
+type Instance = (LabelIdx, Document)
+
 type Label = String
-type Document = [(WordIndex, WordCount)]
+type LabelIdx = Int
+
+type Document = [(WordIdx, WordCount)]
 type DocumentNumber = Int
+
 type Word = String
-type WordIndex = Int
+type WordIdx = Int
 type WordCount = Int
 
 main :: IO ()
@@ -29,14 +33,17 @@ main = do
   test_label_lines <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.label") ReadMode
   test_map_lines   <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.map"  ) ReadMode
   
-  let vocab = listArray (1,(length vocabulary)) vocabulary :: Array Int Word
-      labels = listArray (1,(length labels_ln)) labels_ln  :: Array Int Label
+  let vocab  = listArray (1,(length vocabulary)) vocabulary :: Array Int Word
+      labels = listArray (1,(length labels_ln)) labels_ln   :: Array Int Label
+      
       f = (\x y -> (fst x) == (fst y))           
       train_data_temp = groupBy f (map (break (== ' ')) train_data_lines)
-      test_data_temp  = groupBy f (map (break (== ' '))test_data_lines)
+      test_data_temp  = groupBy f (map (break (== ' ')) test_data_lines )
+      
       trainDocs = map makeDoc train_data_temp 
-      trainData = zip train_label_lines trainDocs :: [Instance]
       testDocs  = map makeDoc test_data_temp
+      
+      trainData = zip train_label_lines trainDocs :: [Instance]
       testData  = zip test_label_lines testDocs   :: [Instance]
 
   print $ numElements vocab
