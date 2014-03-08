cb1edc061664cc031856008e9f585f11f8f0ca9d
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-08 11:54:32 -0700

    /home/tberger/haskell/bayes_classifier/bayesClassifier.hs autocommit from emacs: Sat Mar  8 11:54:32 2014

--- bayes_classifier/bayesClassifier.hs
+++ bayes_classifier/bayesClassifier.hs
@@ -45,8 +45,8 @@ main = do
       trainDocs = map makeDoc train_data_temp 
       testDocs  = map makeDoc test_data_temp
       
-      trainData = (map (read ) train_label_lines) `zip` trainDocs :: [Instance]
-      testData  = (map (read :: String -> Int) test_label_lines)  `zip` testDocs  :: [Instance]
+      trainData = (map read train_label_lines) `zip` trainDocs :: [Instance]
+      testData  = (map read test_label_lines)  `zip` testDocs  :: [Instance]
 
   print $ V.length vocab
   return ()
