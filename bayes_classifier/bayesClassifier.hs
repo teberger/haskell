2d787ea7bda594d54b4699977b8c460173667e20
teberger <tberge01@cs.unm.edu>
2014-03-10 19:45:29 -0400

    /home/repa/haskell/bayes_classifier/bayesClassifier.hs autocommit from emacs

--- bayes_classifier/bayesClassifier.hs
+++ bayes_classifier/bayesClassifier.hs
@@ -58,7 +58,7 @@ main = do
       
       zeros = fromFunction (Z :. nVocab) (\(Z :. wi) -> 0) :: Array D (Z :. WordIdx) Int
       
-      likelyhoods = [foldl' (\x y -> x +^ (snd y)) zeros (filter ((/= i) . fst) trainData) | i <- [0..nLabel]]
+      likelyhoods = foldl' R.++ mzero [foldl' (\x y -> x +^ (snd y)) zeros (filter ((/= i) . fst) trainData) | i <- [0..nLabel]]
 --      likelyhoods is append after I sum across all indexes -> Array U (Z :. labelIdx) (Array U (Z :. WordIdx) Int)
       
       
