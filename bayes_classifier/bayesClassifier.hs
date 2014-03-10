bb0db5d62deee28da18f0094a883baaec0e0e01a
teberger <tberge01@cs.unm.edu>
2014-03-09 19:26:08 -0600

    /home/repa/haskell/bayes_classifier/bayesClassifier.hs autocommit from emacs: Sun Mar  9 19:26:08 2014

--- bayes_classifier/bayesClassifier.hs
+++ bayes_classifier/bayesClassifier.hs
@@ -37,7 +37,7 @@ main = do
 --  test_label_lines <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.label") ReadMode
 --  test_map_lines   <- fmap lines $ hGetContents =<< openFile (dataLoc ++ "test.map"  ) ReadMode
   
-  let vocab = fromListUnboxed (Z :. (length vocabulary :: Int)) vocabulary  
+  let vocab = fromList (Z :. (length vocabulary :: Int)) vocabulary 
 {-      labels = R.fromList labels_ln       
       nVocab = fromIntegral (V.length vocab)
       nLabel = fromIntegral (V.length labels)
