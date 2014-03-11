2c3eda9924246095b0f0e295fb57d6773ba88e88
teberger <tberge01@cs.unm.edu>
2014-03-10 21:11:33 -0400

    /home/repa/haskell/bayes_classifier/bayesClassifier.hs autocommit from emacs

--- bayes_classifier/bayesClassifier.hs
+++ bayes_classifier/bayesClassifier.hs
@@ -62,8 +62,8 @@ main = do
       sums = fromListVector (Z :. nLabel) $ map sumAllS likelyhoods
       
       [trainData'] = computeP $ traverse likelyhoods' (\(Z :. i :. j) -> (Z :. i :. j))
-                                                      (\lp (Z :. i :. j) -> (fromIntegral (lp (Z :. i :. j))) / 
-                                                                             (sums ! (Z :. i)))
+                                                      (\lp (Z :. i :. j) -> ((lp (Z :. i :. j))) / 
+                                                                             fromIntegral (sums ! (Z :. i)))
 
 --      likelyhoods is append after I sum across all indexes -> Array U (Z :. labelIdx) (Array U (Z :. WordIdx) Int)
       
