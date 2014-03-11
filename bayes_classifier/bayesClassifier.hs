749d70d22350827a0882fa1084efe3b6922b67a2
teberger <tberge01@cs.unm.edu>
2014-03-10 21:12:22 -0400

    /home/repa/haskell/bayes_classifier/bayesClassifier.hs autocommit from emacs

--- bayes_classifier/bayesClassifier.hs
+++ bayes_classifier/bayesClassifier.hs
@@ -61,8 +61,8 @@ main = do
       likelyhoods' = reshape (Z :. nLabel :. nVocab) $ foldl' (R.++) (head likelyhoods) (tail likelyhoods)
       sums = fromListVector (Z :. nLabel) $ map sumAllS likelyhoods
       
-      [trainData'] = computeP $ traverse likelyhoods' (\(Z :. i :. j) -> (Z :. i :. j))
-                                                      (\lp (Z :. i :. j) -> (fromIntegral (lp (Z :. i :. j)):: Double) / 
+      trainData' = traverse likelyhoods' (\(Z :. i :. j) -> (Z :. i :. j))
+                                                      (\lp (Z :. i :. j) -> (fromIntegral (lp (Z :. i :. j))) / 
                                                                              fromIntegral (sums ! (Z :. i)))
 
 --      likelyhoods is append after I sum across all indexes -> Array U (Z :. labelIdx) (Array U (Z :. WordIdx) Int)
