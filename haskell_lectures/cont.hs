a30207aedf252ed6051078e4af4134112c460cef
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-19 20:19:22 -0600

    /home/tberger/haskell/haskell_lectures/cont.hs autocommit from emacs: Wed Mar 19 20:19:22 2014

--- haskell_lectures/cont.hs
+++ haskell_lectures/cont.hs
@@ -3,4 +3,4 @@ module Main where
 import Control.Monad.Trans.Cont
 
 square :: Int -> (Int -> a) -> a
-square x foo = foo $ \x1 -> (*) x1 $ \x2 -> x2
+square x foo = foo $ (\x1 -> (*) x1 $ \x2 -> x2)
