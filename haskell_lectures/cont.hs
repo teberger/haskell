46c7cd82cda89f822647f2a634123627d4c97adc
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-19 20:14:36 -0600

    /home/tberger/haskell/haskell_lectures/cont.hs autocommit from emacs: Wed Mar 19 20:14:36 2014

--- haskell_lectures/cont.hs
+++ haskell_lectures/cont.hs
@@ -3,7 +3,9 @@ module Main where
 import Control.Monad.Trans.Cont
 
 square :: Int -> Int
-square x = ((*) x) x
+square x = id ((*) x) x
 
 fib :: Int -> Int
-fib x = if (x < 2) then x else (fib (x-1)) + (fib (x-2))
+fib x = if (x < 2) 
+        then x 
+        else (fib (x-1)) + (fib (x-2))
