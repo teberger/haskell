a7900dcf0e7936398c243b2ff9551627db417b1e
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-19 20:19:11 -0600

    /home/tberger/haskell/haskell_lectures/cont.hs autocommit from emacs: Wed Mar 19 20:19:11 2014

--- haskell_lectures/cont.hs
+++ haskell_lectures/cont.hs
@@ -3,9 +3,4 @@ module Main where
 import Control.Monad.Trans.Cont
 
 square :: Int -> (Int -> a) -> a
-square x foo = foo (((*) x) x)
-
-fib :: Int -> (Int -> a) -> a
-fib x foo = if (x < 2) 
-            then x 
-            else (+) (fib (x-1)) (fib (x-2))
+square x foo = foo $ \x1 -> (*) x1 $ \x2 -> x2
