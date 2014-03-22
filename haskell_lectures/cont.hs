615fcbd983d6f62099b79b21559c38831223802b
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-21 20:34:14 -0600

    /home/tberger/haskell/haskell_lectures/cont.hs autocommit from emacs: Fri Mar 21 20:34:14 2014

--- haskell_lectures/cont.hs
+++ haskell_lectures/cont.hs
@@ -28,6 +28,8 @@ add''' = (\f -> (\x y -> f x y)) (+)
 
 op = \operator -> (\x -> (\y -> operator x y))
 
+op' = \operator -> (\x -> (\x -> \context -> context $ operator x y))
+
 --add_cont :: Int -> Int -> ((Int -> a) -> a)
 add_cont = \fplus -> (\x y -> (\sShow -> sShow (fplus x y)))
 
