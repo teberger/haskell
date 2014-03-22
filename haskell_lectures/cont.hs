955ba3bc81e1069f85244b5180d34a47628ce84a
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-22 08:51:39 -0600

    /home/tberger/haskell/haskell_lectures/cont.hs autocommit from emacs: Sat Mar 22 08:51:39 2014

--- haskell_lectures/cont.hs
+++ haskell_lectures/cont.hs
@@ -29,8 +29,8 @@ add''' = (\f -> (\x y -> f x y)) (+)
 op = \operator -> (\x -> (\y -> operator x y))
 
 op' = \operator -> $ (\x -> 
-                   $ \y -> 
-                   $ \context -> context $ operator x y)
+                   $ (\y -> 
+                   $ \context -> context $ operator x y))
 
 add_cont = op' (+)
 mult_cont = op' (*)
