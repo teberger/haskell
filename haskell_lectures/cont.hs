fa7eade5666046deaf5c0c09ac278adcd13dd761
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-21 19:34:34 -0600

    /home/tberger/haskell/haskell_lectures/cont.hs autocommit from emacs: Fri Mar 21 19:34:34 2014

--- haskell_lectures/cont.hs
+++ haskell_lectures/cont.hs
@@ -4,4 +4,4 @@ import Control.Monad.Trans.Cont
 import Control.Applicative
 
 square :: Int -> Int
-square = \x -> (*) $ \x1 -> id $ \x2 -> x <*> id $ \x3 -> x 
+square = \x -> (*) $ \x1 -> id $ \x2 -> x $ \x3 -> x 
