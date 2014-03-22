5aeebdd80c696ad9b0ba0ac7ce8e0ea51078214d
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-21 19:46:39 -0600

    /home/tberger/haskell/haskell_lectures/cont.hs autocommit from emacs: Fri Mar 21 19:46:39 2014

--- haskell_lectures/cont.hs
+++ haskell_lectures/cont.hs
@@ -3,5 +3,5 @@ module Main where
 import Control.Monad.Trans.Cont
 import Control.Applicative
 
-square :: Int -> Int
-square x = div $ \x1 x2 -> x
+square :: Int -> Int -> ((a -> Int) -> Int)
+square x = div x $ \y -> y x
