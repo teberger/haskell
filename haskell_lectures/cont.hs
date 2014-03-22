bfab407c91168045636a0eee37da56a47df0925f
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-21 19:40:54 -0600

    /home/tberger/haskell/haskell_lectures/cont.hs autocommit from emacs: Fri Mar 21 19:40:54 2014

--- haskell_lectures/cont.hs
+++ haskell_lectures/cont.hs
@@ -3,5 +3,5 @@ module Main where
 import Control.Monad.Trans.Cont
 import Control.Applicative
 
-square :: Int -> (t0 -> Int) -> t0 -> Int
+square :: Int -> (a -> Int) -> (Int -> Int) 
 square = \x -> (*) $ \x1 -> x1 x  --(\x1 -> x1 x x)
