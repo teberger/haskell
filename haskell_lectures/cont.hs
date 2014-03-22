92b7505028f8651db6cdfddf76acaf557d1cc433
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-21 20:14:46 -0600

    /home/tberger/haskell/haskell_lectures/cont.hs autocommit from emacs: Fri Mar 21 20:14:46 2014

--- haskell_lectures/cont.hs
+++ haskell_lectures/cont.hs
@@ -21,8 +21,8 @@ add1_cps x = \f -> f (x + 1)
 add1_cps' :: ((Int -> Int) -> a) -> a
 add1_cps' = \f -> f (+ 1)
 
-add1_cps'' :: (((Int -> a) -> a) -> ((Int -> a) -> a)) -> a
-add1_cps'' = \f -> f $ \f0 -> f0 (+ 1)
+--add1_cps'' :: (((Int -> a) -> a) -> ((Int -> a) -> a)) -> a
+--add1_cps'' = \f -> f $ \f0 -> f0 (+ 1)
 
 add_cont :: Int -> Int -> ((Int -> a) -> a)
 add_cont x y = \f -> f (x + y)
