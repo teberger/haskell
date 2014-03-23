8bddbd02003d4ca000be8d640569c9a49a56b4cd
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-23 10:17:59 -0600

    /home/tberger/haskell/haskell_lectures/cont.hs autocommit from emacs: Sun Mar 23 10:17:59 2014

--- haskell_lectures/cont.hs
+++ haskell_lectures/cont.hs
@@ -90,8 +90,6 @@ fib'' n = callCC $ \k -> do
 
 -- k is our continuation function. It acts as a return from
 -- the continuation monad.
-squareroot n = callCC $ \k -> do
+realRoot n = callCC $ \k -> do
   when (n < 0) $ k "Error. N is < 0 and does not have a real square root"
   return (show $ sqrt n)
-  
-  
