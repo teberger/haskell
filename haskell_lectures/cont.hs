4882649db01a7c3a0fd3ad52270c67ced392e3b8
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-23 10:24:46 -0600

    /home/tberger/haskell/haskell_lectures/cont.hs autocommit from emacs: Sun Mar 23 10:24:46 2014

--- haskell_lectures/cont.hs
+++ haskell_lectures/cont.hs
@@ -93,5 +93,4 @@ fib'' n = callCC $ \k -> do
 realRoot n = callCC $ \k -> do
   when (n < 0) $ k "n cannot be less than 0. It does not have a real root"
   k (show $ sqrt n)
-  return
-
+  return "error"
