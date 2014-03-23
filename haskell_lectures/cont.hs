f8cdd1a4bacf53770c6e126d75187f85fb9fd8ac
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-23 11:17:39 -0600

    /home/tberger/haskell/haskell_lectures/cont.hs autocommit from emacs: Sun Mar 23 11:17:39 2014

--- haskell_lectures/cont.hs
+++ haskell_lectures/cont.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Control.Monad.Trans.Cont
 import Control.Applicative
-import Control.Monad (when)
+import Control.Monad (when, join)
 
 -- Continuation is typed as (a -> r) -> r
 -- Cont a r
