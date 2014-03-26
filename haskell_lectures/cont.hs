7c3f1cf59aed6a460806ec2d3ed5c07052031c27
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-25 18:17:13 -0600

    /home/tberger/haskell/haskell_lectures/cont.hs autocommit from emacs: Tue Mar 25 18:17:13 2014

--- haskell_lectures/cont.hs
+++ haskell_lectures/cont.hs
@@ -167,5 +167,5 @@ pinteger = do
 
 --pinteger' :: ((r' -> r') -> m r -> m r) -> (r' -> r') -> ContT r Parser Int -> ContT r Parser Int
 pinteger' = callCC $ \k -> do
-  lift (parse many "") >>= k
+  lift (parse many) >>= k
 -- ????
