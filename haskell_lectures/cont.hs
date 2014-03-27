abac60f854854af75e016a30d540c9e6fea5bd34
Taylor E Berger <tberge01@cs.unm.edu>
2014-03-27 11:14:24 -0600

    /home/tberger/haskell/haskell_lectures/cont.hs autocommit from emacs: Thu Mar 27 11:14:24 2014

--- haskell_lectures/cont.hs
+++ haskell_lectures/cont.hs
@@ -1,10 +1,10 @@
 module Main where
 
 import Control.Monad.Trans.Cont
-import Control.Monad.Trans
+--import Control.Monad.Trans
 import Control.Monad (when, join)
 import Data.Char
---import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec
 
 
 -- Continuation is typed as (a -> r) -> r
